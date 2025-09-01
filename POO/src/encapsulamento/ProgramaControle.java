d463258b7ca277f9cb79c90519d9d72ea64393e2
Anderson Freires <55098476+dre36dre@users.noreply.github.com>
2025-08-31 22:08:03 -0300

    Encapsulando a classe

---
+++ POO/src/encapsulamento/ProgramaControle.java
@@ -0,0 +1,14 @@
+package encapsulamento;
+
+public class ProgramaControle {
+
+	public static void main(String[] args) {
+		ControleRemoto c= new ControleRemoto();
+		c.ligar();
+		c.abrirMenu();
+		c.maisVolume();
+		c.play();
+		c.fecharMenu();
+	}
+
+}

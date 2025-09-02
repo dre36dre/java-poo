f53c646b7fb321eaa9340d34674513d955fc939e
Anderson Freires <dre.son36@gmail.com>
2025-09-02 10:47:15 -0300

    Composição em java

---
+++ POO/src/composicao/ProgramaLutador.java
@@ -0,0 +1,8 @@
+package composicao;
+
+public class ProgramaLutador {
+public static void main(String[] args) {
+	Lutador l=new Lutador("Dre", "Italiana", 46, 75, 1.75f, 7, 1, 3);
+	l.apresentar();
+}
+}

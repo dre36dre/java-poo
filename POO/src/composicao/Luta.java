ec2b84ca35f3d7d0fc7b1e6570c8be5389abef55
Anderson Freires <55098476+dre36dre@users.noreply.github.com>
2025-09-03 22:20:36 -0300

    Criando uma agregação

--- POO/src/composicao/Luta.java
+++ POO/src/composicao/Luta.java
@@ -1,16 +1,56 @@
 package composicao;
 
+import java.util.Random;
+
 public class Luta {
+
 private Lutador desafiado;
 private Lutador desafiante;
-//private 
+private int rounds;
+private boolean aprovada;
 
 
-public void marcarLutar() {
+
+
+public Lutador getDesafiado() {
+	return desafiado;
+}
+public void setDesafiado(Lutador desafiado) {
+	this.desafiado = desafiado;
+}
+public Lutador getDesafiante() {
+	return desafiante;
+}
+public void setDesafiante(Lutador desafiante) {
+	this.desafiante = desafiante;
+}
+public int getRounds() {
+	return rounds;
+}
+public void setRounds(int rounds) {
+	this.rounds = rounds;
+}
+public boolean isAprovada() {
+	return aprovada;
+}
+public void setAprovada(boolean aprovada) {
+	this.aprovada = aprovada;
+}
+
+public void marcarLutar(Lutador l1, Lutador l2) {
+	if(l1.getCategoria() == l2.getCategoria()) && ()
 	
 }
 public void lutar() {
-	
+	if(aprovada) {
+		desafiado.apresentar();
+		desafiante.apresentar();
+		vencedor=Random(2);
+		sw
+		
+	}else {
+		System.out.println("Luta não pode acontecer");
+	}
 }
 
 }
--- POO/src/composicao/ProgramaLutador.java
+++ POO/src/composicao/ProgramaLutador.java
@@ -2,7 +2,10 @@ package composicao;
 
 public class ProgramaLutador {
 public static void main(String[] args) {
-	Lutador l=new Lutador("Dre", "Italiana", 46, 75, 1.75f, 7, 1, 3);
-	l.apresentar();
+	Lutador l[6]= new Lutador(null, null, 0, 0, 0, 0, 0, 0)
+	 l[0]=new Lutador("Dre", "Italiana", 46, 75, 1.75f, 7, 1, 3);
+	l[0].apresentar();
+	
+	Luta uec01=new Luta();
 }
 }
